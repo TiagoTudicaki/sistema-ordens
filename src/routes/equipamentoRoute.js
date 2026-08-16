97fbee73095b1ccad32b2f325a26a31b9071349f
Tiago <tiago.h.tudicaki@gmail.com>
2026-08-16 17:35:12 -0300

    fix: corrige metodo HTTP de PUT para PATCH em atualizar
    A rota de atualizar equipamentos estava registrada como PUT, mas
    o comportamento implementado eh de atualizacao parcial (so os
    campos enviados sao alterados), o que corresponde semanticamente
    a PATCH, nao PUT.

--- src/routes/equipamentoRoute.js
+++ src/routes/equipamentoRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post("/equipamentos", equipamentoController.criar);
 router.get("/equipamentos", equipamentoController.listar);
 router.get("/equipamentos/:id", equipamentoController.buscarPorId);
-router.put("/equipamentos/:id", equipamentoController.atualizar);
+router.patch("/equipamentos/:id", equipamentoController.atualizar);
 router.delete("/equipamentos/:id", equipamentoController.excluir);
 
 module.exports = router;
