be99f483de0f104e6d0539db5e5b4de8e3637db8
Tiago <tiago.h.tudicaki@gmail.com>
2026-08-30 13:36:00 -0300

    refactor: migra criar e atualizar de equipamentos para usar validarCampoEnum genérica

--- src/services/equipamentoService.js
+++ src/services/equipamentoService.js
@@ -4,32 +4,27 @@ const {
   validarIdentificador,
   validarCamposVazios,
   validarTextoSimples,
-  validarCampoEnumTipo,
+  validarCampoEnum,
   validarTextoSimplesOpcional,
   validarLocal,
   validarMarca,
   validarModelo,
   validarSerie,
 } = require("../utils/validarCampos");
-const {  
+const {
   padronizarTexto,
   padronizarCapacidade,
   padronizarTipoDeGas,
   padronizarModelo,
   padronizarSerie,
-  
 } = require("../utils/padronizarDados");
 
-const {consultaFiltrada} = require("../utils/filtragemDeConsulta");
+const { consultaFiltrada } = require("../utils/filtragemDeConsulta");
 const valoresTrimados = require("../utils/sanitizarDados");
 
 const equipamentoService = {
   async criar(dados) {
-
-    
     const camposExistentes = consultaFiltrada(dados);
-
-    
 
     const {
       cliente_id,
@@ -43,123 +38,137 @@ const equipamentoService = {
       tipo_gas,
     } = camposExistentes;
 
-    const camposVazios = validarCamposVazios({cliente_id, local, identificador});
-    
+    const camposVazios = validarCamposVazios({
+      cliente_id,
+      local,
+      identificador,
+    });
 
-    if(camposVazios.length > 0){
-      throw new Error(`O(s) campo(s) ${camposVazios.join(", ")} são obrigátorio(s)`);
+    if (camposVazios.length > 0) {
+      throw new Error(
+        `O(s) campo(s) ${camposVazios.join(", ")} são obrigátorio(s)`,
+      );
     }
 
     //----Cliente_id-----
 
     const cliente_idValido = validarId(cliente_id);
 
-   
+    //------Local-------
 
+    const localValido = /^[A-Za-zÀ-ÿ\s']+$/.test(local);
 
-   //------Local-------
-
-   const localValido = /^[A-Za-zÀ-ÿ\s']+$/.test(local);
-
-   if(!localValido){
-    throw new Error("Local deve possuir apenas letra, espaço, apostrofo");
-   }
-
-   //-----Identificador------
-
-   const identificadorTexto = String(identificador);
-
-   if(identificadorTexto.length != 2){
-    throw new Error("Identificador deve possuir 2 digitos");
-   }
-   const identificadorValido = /^[0-9]+$/.test(identificadorTexto);
-
-   if(!identificadorValido){
-    throw new Error("Identificador deve possuir apenas números");
-   }
-
-   //----Marca------
-
-   if(marca != null){
-    const marcaValida = /^[A-Za-zÀ-ÿ\s']+$/.test(marca);
-    if(!marcaValida){
-      throw new Error("Marca de possuir apenas letra,espaço e apostrofos");
+    if (!localValido) {
+      throw new Error("Local deve possuir apenas letra, espaço, apostrofo");
     }
-   }
 
-   //-----Modelo--------
+    //-----Identificador------
 
-   if(modelo != null){
-    const modeloValido = /^[A-Za-zÀ-ÿ0-9\s-]+$/.test(modelo);
-    if(!modeloValido){
-      throw new Error("Modelo não deve possuir caracteres especiais ou ponto");
+    const identificadorTexto = String(identificador);
+
+    if (identificadorTexto.length != 2) {
+      throw new Error("Identificador deve possuir 2 digitos");
     }
-   }
+    const identificadorValido = /^[0-9]+$/.test(identificadorTexto);
 
-   //------Serie------
-
-   if(serie != null){
-    const serieValida =  /^[A-Za-zÀ-ÿ0-9\s-]+$/.test(serie);
-    if(!serieValida){
-      throw new Error("Serie não dever possuir caracteres especiais ou ponto");
-      
+    if (!identificadorValido) {
+      throw new Error("Identificador deve possuir apenas números");
     }
-   }
 
-   //-----Capacidade_btu------
+    //----Marca------
 
-   if(capacidade_btu != null){
-    const capacidade_btuValido = /^[A-Za-zÀ-ÿ0-9\s.]+$/.test(capacidade_btu);
-    if(!capacidade_btuValido){
-      throw new Error("Capacidade não possuir caracteres especiais e traços");
+    if (marca != null) {
+      const marcaValida = /^[A-Za-zÀ-ÿ\s']+$/.test(marca);
+      if (!marcaValida) {
+        throw new Error("Marca de possuir apenas letra,espaço e apostrofos");
+      }
     }
-   }
 
-   //----Tipo_gas------
+    //-----Modelo--------
 
-   if(tipo_gas != null){
-    const tipo_gasValido = /^[A-Za-zÀ-ÿ0-9\-]+$/.test(tipo_gas);
-    if(!tipo_gasValido){
-      throw new Error("O tipo do gas não deve possuir espaço ou caracteres especiais ou pontos");
-   }
-  }
-   const camposComDados = {};
+    if (modelo != null) {
+      const modeloValido = /^[A-Za-zÀ-ÿ0-9\s-]+$/.test(modelo);
+      if (!modeloValido) {
+        throw new Error(
+          "Modelo não deve possuir caracteres especiais ou ponto",
+        );
+      }
+    }
 
-   camposComDados.cliente_id = cliente_idValido;
-   if(tipo != null){
-    camposComDados.tipo = validarCampoEnumTipo(tipo);
-   }
-   camposComDados.local = padronizarTexto(local);
-   camposComDados.identificador = identificadorTexto;
-   if(marca != null){
-    camposComDados.marca = padronizarTexto(marca);
-   }
-   if(modelo != null){
-    camposComDados.modelo = modelo;
-   }
-   if(serie != null){
-    camposComDados.serie = serie;
-   }
-   if(capacidade_btu != null){
-    camposComDados.capacidade_btu = padronizarCapacidade(capacidade_btu);
-   }
-   if(tipo_gas != null){
-    camposComDados.tipo_gas = padronizarTipoDeGas(tipo_gas);
-   }
+    //------Serie------
 
+    if (serie != null) {
+      const serieValida = /^[A-Za-zÀ-ÿ0-9\s-]+$/.test(serie);
+      if (!serieValida) {
+        throw new Error(
+          "Serie não dever possuir caracteres especiais ou ponto",
+        );
+      }
+    }
 
-   const equipamento = await equipamentoModel.criar(camposComDados);
-   return equipamento;
-   
-   },
-  
+    //-----Capacidade_btu------
+
+    if (capacidade_btu != null) {
+      const capacidade_btuValido = /^[A-Za-zÀ-ÿ0-9\s.]+$/.test(capacidade_btu);
+      if (!capacidade_btuValido) {
+        throw new Error("Capacidade não possuir caracteres especiais e traços");
+      }
+    }
+
+    //----Tipo_gas------
+
+    if (tipo_gas != null) {
+      const tipo_gasValido = /^[A-Za-zÀ-ÿ0-9\-]+$/.test(tipo_gas);
+      if (!tipo_gasValido) {
+        throw new Error(
+          "O tipo do gas não deve possuir espaço ou caracteres especiais ou pontos",
+        );
+      }
+    }
+    const camposComDados = {};
+
+    const tipoDeEquipamento = [
+      "acj",
+      "hi-wall",
+      "piso-teto",
+      "multi-split",
+      "cassete",
+      "self-contained",
+      "built-in",
+      "vrf",
+      "fan-coil",
+      "roof-top",
+    ];
+    camposComDados.cliente_id = cliente_idValido;
+    if (tipo != null) {
+      camposComDados.tipo = validarCampoEnum(tipo, tipoDeEquipamento);
+    }
+    camposComDados.local = padronizarTexto(local);
+    camposComDados.identificador = identificadorTexto;
+    if (marca != null) {
+      camposComDados.marca = padronizarTexto(marca);
+    }
+    if (modelo != null) {
+      camposComDados.modelo = modelo;
+    }
+    if (serie != null) {
+      camposComDados.serie = serie;
+    }
+    if (capacidade_btu != null) {
+      camposComDados.capacidade_btu = padronizarCapacidade(capacidade_btu);
+    }
+    if (tipo_gas != null) {
+      camposComDados.tipo_gas = padronizarTipoDeGas(tipo_gas);
+    }
+
+    const equipamento = await equipamentoModel.criar(camposComDados);
+    return equipamento;
+  },
 
   async listar(camposExistentes) {
-
-  
-  const equipamentos = await equipamentoModel.listar(camposExistentes);
-  return equipamentos;
-},
+    const equipamentos = await equipamentoModel.listar(camposExistentes);
+    return equipamentos;
+  },
 
   async buscarPorId(id) {
     const equipamento = await equipamentoModel.buscarPorId(id);
@@ -172,67 +181,80 @@ const equipamentoService = {
   },
 
   async atualizar(id, camposExistentes) {
-    
-    if(Object.keys(camposExistentes).length === 0){
+    if (Object.keys(camposExistentes).length === 0) {
       const erro = new Error("É necessário alterar pelos menos um campo");
       erro.status = 400;
       throw erro;
-    } 
+    }
 
     const camposTrimados = valoresTrimados(camposExistentes);
-    
+
     const dados = {};
 
-    if(camposTrimados.cliente_id != null){
+    const tipoDeEquipamento = [
+      "acj",
+      "hi-wall",
+      "piso-teto",
+      "multi-split",
+      "cassete",
+      "self-contained",
+      "built-in",
+      "vrf",
+      "fan-coil",
+      "roof-top",
+    ];
+
+    if (camposTrimados.cliente_id != null) {
       dados.cliente_id = validarId(camposTrimados.cliente_id);
     }
 
-    if(camposTrimados.tipo !=  null){
-      dados.tipo = validarCampoEnumTipo(camposTrimados.tipo);
+    if (camposTrimados.tipo != null) {
+      dados.tipo = validarCampoEnum(camposTrimados.tipo, tipoDeEquipamento);
     }
 
-    if(camposTrimados.local != null){
+    if (camposTrimados.local != null) {
       dados.local = padronizarTexto(validarLocal(camposTrimados.local));
     }
 
-    if(camposTrimados.identificador != null){
+    if (camposTrimados.identificador != null) {
       dados.identificador = validarIdentificador(camposTrimados.identificador);
     }
 
-    if(camposTrimados.marca != null){
+    if (camposTrimados.marca != null) {
       dados.marca = padronizarTexto(validarMarca(camposTrimados.marca));
     }
 
-    if(camposTrimados.modelo != null){
+    if (camposTrimados.modelo != null) {
       dados.modelo = validarModelo(camposTrimados.modelo);
     }
 
-    if(camposTrimados.serie != null){
+    if (camposTrimados.serie != null) {
       dados.serie = validarSerie(camposTrimados.serie);
-    } 
-
-    if(camposTrimados.capacidade_btu != null){
-      dados.capacidade_btu = padronizarCapacidade(camposTrimados.capacidade_btu);
     }
 
-    if(camposTrimados.tipo_gas != null){
+    if (camposTrimados.capacidade_btu != null) {
+      dados.capacidade_btu = padronizarCapacidade(
+        camposTrimados.capacidade_btu,
+      );
+    }
+
+    if (camposTrimados.tipo_gas != null) {
       dados.tipo_gas = padronizarTipoDeGas(camposTrimados.tipo_gas);
     }
 
     const equipamento = await equipamentoModel.atualizar(id, dados);
     return equipamento;
-
   },
 
- async excluir(id) {
-  const resultado = await equipamentoModel.excluir(id);
-  if (resultado.affectedRows === 0) {
-    const erro = new Error("Equipamento não encontrado");
-    erro.status = 404;
-    throw erro;
-  }
-  return resultado;
-},
+  async excluir(id) {
+    const resultado = await equipamentoModel.excluir(id);
+    if (resultado.affectedRows === 0) {
+      const erro = new Error("Equipamento não encontrado");
+      erro.status = 404;
+      throw erro;
+    }
+    return resultado;
+  },
 };
 
 module.exports = equipamentoService;
